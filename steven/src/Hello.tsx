9e36383b4edc17a95875f12e00b71925888ab371
steven31518 <107013204+steven31518@users.noreply.github.com>
2023-06-21 17:31:09 +0800

    Update Hello.tsx

--- steven/src/Hello.tsx
+++ steven/src/Hello.tsx
@@ -18,11 +18,16 @@ const Hello = () => {
     const { name, age, job } = person;
     return `Hello,my name is ${name},I'm ${age} years old. my job is ${job}`;
   };
+  type calculatorType = (...restProps: number[]) => number;
+  const sum: calculatorType = (...restProps) => {
+    return restProps.reduce((a, b) => a + b);
+  };
+
   return (
     <div>
       {Bot({ id: 1, name: "Steven", age: 18, job: "programer" })}
       <br />
-      {typeof mySum(1, 2)}
+      {sum(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)}
     </div>
   );
 };
