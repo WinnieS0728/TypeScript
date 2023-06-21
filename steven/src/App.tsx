b674de4a0f6b7c95cae6b5948d88f3369b4bc7ac
steven31518 <107013204+steven31518@users.noreply.github.com>
2023-06-21 17:26:53 +0800

    learn typescript

--- steven/src/App.tsx
+++ steven/src/App.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import Hello from "./Hello";
+
 function App() {
   return (
     <div className="App">
--- steven/src/Hello.tsx
+++ steven/src/Hello.tsx
@@ -1,17 +1,28 @@
 const Hello = () => {
-  
   const sayHello = (person: string) => {
     return `Hello, ${person}`;
   };
   const saySomeThing = (someThing: any) => {
     return someThing;
   };
-
-  const self = () => {};
+  const mySum = (a: number, b: number): number => {
+    return a + b;
+  };
+  interface Person {
+    readonly id: number;
+    name: string;
+    age?: number;
+    [propName: string]: string | number | undefined;
+  }
+  const Bot: (person: Person) => string = (person) => {
+    const { name, age, job } = person;
+    return `Hello,my name is ${name},I'm ${age} years old. my job is ${job}`;
+  };
   return (
     <div>
-      {sayHello("Steven") + `,you are learning ` + saySomeThing(true)}
+      {Bot({ id: 1, name: "Steven", age: 18, job: "programer" })}
       <br />
+      {typeof mySum(1, 2)}
     </div>
   );
 };
