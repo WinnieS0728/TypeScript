34021a3e408130fd74ca68e50d515bc4aa7d2c6d
steven31518 <107013204+steven31518@users.noreply.github.com>
2023-06-27 17:29:51 +0800

    practice 6.27

--- steven/src/Hello.tsx
+++ steven/src/Hello.tsx
@@ -1,3 +1,6 @@
+import { useEffect, useState } from "react";
+import axios from "axios";
+import { get } from "http";
 const Hello = () => {
   // 基本型別
   let isDone: boolean = false;
@@ -12,6 +15,7 @@ const Hello = () => {
     ["1", "2"],
     ["3", "4"],
   ];
+
   //元祖
   let tuple: [string, number, boolean] = ["1", 2, true];
   //列舉
@@ -45,6 +49,83 @@ const Hello = () => {
     age: 18,
     isPremium: true,
   };
+  //----unknown & any-----
+  type Data = {
+    userId: number;
+    id: number;
+    title: string;
+    completed: boolean;
+  };
+  const getData = async () => {
+    const res = await fetch("https://jsonplaceholder.typicode.com/todos/1");
+    const data = (await res.json()) as Data;
+    console.log(data);
+  };
+
+  const data1: Data = {
+    userId: 1,
+    id: 1,
+    title: "delectus aut autem",
+    completed: false,
+  };
+  type Beta = {
+    name: string;
+  };
+  //假設data1是從伺服器取得的資料，但是我們不確定資料的型別，所以先用unknown
+  const beta = data1 as unknown as Beta;
+  //------class--------------------
+  //private
+  //public
+  //protected
+
+  class Live {
+    roomName: string;
+    private id: string;
+    protected status: number;
+    constructor(rn: string, identify: string, state: number) {
+      this.roomName = rn;
+      this.id = identify;
+      this.status = state;
+    }
+  }
+  class Live2 extends Live {
+    constructor(rn: string, identify: string, state: number) {
+      super(rn, identify, state);
+    }
+    start() {}
+  }
+
+  const live = new Live("Steven", "00001", 1);
+  const liveSecond = new Live2("Sliven", "00002", 0);
+  //console.log(live.id) //error private
+  console.log(live, liveSecond);
+  interface CarInterface {
+    name: string;
+    run(): void;
+  }
+
+  class car implements CarInterface {
+    name: string;
+    constructor(name: string) {
+      this.name = name;
+    }
+    run() {
+      console.log("car is running");
+    }
+  }
+  //-----------泛型----------------
+  //泛型是指在定義函式、介面或類別的時候，不預先指定具體的型別，而是在使用的時候再指定型別的一種特性。
+
+  function print<T>(data: T) {
+    console.log("data", data);
+  }
+
+  print<string>("hello"); //各種不同類型
+  print<number>(123);
+  print<boolean>(true);
+//--------------untilities----------------
+
+
   //引數預設值
   const buildName = (firstName: string, lastName: string = "cat"): string => {
     return firstName + " " + lastName;
@@ -89,9 +170,29 @@ const Hello = () => {
     });
     return num;
   };
+
   interface visitData {
-    [propsName: string]: number;
+    [propsName: string]: number | string | boolean;
   }
+  let ajaxData: visitData[] = [
+    { createData: "2021-01-01", value: 123, isPremium: true },
+    { createData: "2021-01-02", value: 122, isPremium: false },
+    { createData: "2021-01-03", value: 121, isPremium: true },
+  ];
+
+  type ajaxType = (arr: visitData[]) => number;
+  const sumAjaxDataValue: ajaxType = (arr) => {
+    let num: number = 0;
+    arr.forEach((item) => {
+      const { value } = item;
+      num += value as number;
+    });
+    return num;
+  };
+
+  useEffect(() => {
+    getData();
+  }, []);
   return (
     <div>
       <br />
@@ -100,6 +201,9 @@ const Hello = () => {
       {getDept(allDept)}
       <br />
       {getNum(allNumObject)}
+      <br />
+      {sumAjaxDataValue(ajaxData)}
+      <br />
     </div>
   );
 };
