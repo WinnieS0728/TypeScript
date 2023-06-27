8bba3e5847350eab0bb021e7c61b0bc7068daab6
steven31518 <107013204+steven31518@users.noreply.github.com>
2023-06-27 12:26:58 +0800

    Practice

--- steven/src/Hello.tsx
+++ steven/src/Hello.tsx
@@ -1,49 +1,54 @@
-import { get } from "http";
-
 const Hello = () => {
-  interface Person {
-    readonly id: number;
+  // 基本型別
+  let isDone: boolean = false;
+  let decimal: number = 6;
+  let color: string = "blue";
+
+  //陣列
+  let list: number[] = [1, 2, 3];
+  let list2: Array<number> = [1, 2, 3];
+  let x: [string, number];
+  let arr2: string[][] = [
+    ["1", "2"],
+    ["3", "4"],
+  ];
+  //元祖
+  let tuple: [string, number, boolean] = ["1", 2, true];
+  //列舉
+  enum LiveStatus {
+    SUCCESS = 0,
+    FAIL = -1,
+    STREAMING = 1,
+  }
+  const status = LiveStatus.SUCCESS;
+  console.log(status); //0
+  //Union
+  let aaa: string | number;
+  aaa = 1;
+  aaa = "1";
+  //type
+  type A = string | number;
+  let a1: A = 1;
+  a1 = 999;
+
+  //interface (可擴充) 類別type(不可擴充)
+  interface User {
     name: string;
-    age?: number;
-    [propName: string]: string | number | undefined;
+    age: number;
   }
-  const Bot: (person: Person) => string = (person) => {
-    const { name, age, job } = person;
-    return `Hello,my name is ${name},I'm ${age} years old. my job is ${job}`;
-  };
-  type calculatorType = (...restProps: number[]) => number;
-  const sum: calculatorType = (...restProps) => {
-    return restProps.reduce((a, b) => a + b);
-  };
-
-  type Name = string;
-  type NameResolver = () => string;
-  type NameOrResolver = Name | NameResolver;
-  const getName = (n: NameOrResolver): Name => {
-    if (typeof n === "string") {
-      return n;
-    } else {
-      return n();
-    }
-  };
-
-  // type cal = (x: number, y: number) => number;
-  // let mySum: cal = (x, y) => {
-  //   return x + y;
-  // };
-
-  interface cal {
-    (x: number, y: number): number;
+  interface User {
+    isPremium?: boolean;
   }
-  const mySum: cal = (x, y) => {
-    return x + y;
+  //---------object----------
+  const obj: User = {
+    name: "Steven",
+    age: 18,
+    isPremium: true,
   };
-
   //引數預設值
   const buildName = (firstName: string, lastName: string = "cat"): string => {
     return firstName + " " + lastName;
   };
-
   type calLength = (something: string | number) => number;
   const getLength: calLength = (something) => {
     if ((something as string).length) {
@@ -56,7 +61,7 @@ const Hello = () => {
     [propName: string]: string;
   }
   const allDept: stringObject[] = [
-    { name: "Steven", dept: "IT"},
+    { name: "Steven", dept: "IT" },
     { name: "John", dept: "Research" },
     { name: "Mary", dept: "Sales" },
   ];
@@ -68,19 +73,33 @@ const Hello = () => {
     });
     return dept;
   };
+  interface numObject {
+    [propName: string]: number;
+  }
+  const allNumObject: numObject[] = [
+    { no: 1, quentity: 18 },
+    { no: 2, quentity: 20 },
+    { no: 3, quentity: 22 },
+  ];
+  type numType = (arr: numObject[]) => number;
+  const getNum: numType = (arr) => {
+    let num: number = 0;
+    arr.forEach((item) => {
+      num += item.quentity;
+    });
+    return num;
+  };
+  interface visitData {
+    [propsName: string]: number;
+  }
   return (
     <div>
-      {Bot({ id: 1, name: "Steven", age: 18, job: "programer" })}
-      <br />
-      {sum(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)}
-      <br />
-      {getName("Steven")}
-      <br />
-      {mySum(1, 2)}
       <br />
       {getLength(1232)}
       <br />
       {getDept(allDept)}
+      <br />
+      {getNum(allNumObject)}
     </div>
   );
 };
