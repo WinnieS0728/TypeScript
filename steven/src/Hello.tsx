dd94fbf6e45cbc2ce87d8d2f3d366a7af1d37dd7
steven31518 <107013204+steven31518@users.noreply.github.com>
2023-06-27 08:43:43 +0800

    practice

--- steven/src/Hello.tsx
+++ steven/src/Hello.tsx
@@ -1,13 +1,6 @@
+import { get } from "http";
+
 const Hello = () => {
-  const sayHello = (person: string) => {
-    return `Hello, ${person}`;
-  };
-  const saySomeThing = (someThing: any) => {
-    return someThing;
-  };
-  const mySum = (a: number, b: number): number => {
-    return a + b;
-  };
   interface Person {
     readonly id: number;
     name: string;
@@ -23,11 +16,71 @@ const Hello = () => {
     return restProps.reduce((a, b) => a + b);
   };
 
+  type Name = string;
+  type NameResolver = () => string;
+  type NameOrResolver = Name | NameResolver;
+  const getName = (n: NameOrResolver): Name => {
+    if (typeof n === "string") {
+      return n;
+    } else {
+      return n();
+    }
+  };
+
+  // type cal = (x: number, y: number) => number;
+  // let mySum: cal = (x, y) => {
+  //   return x + y;
+  // };
+
+  interface cal {
+    (x: number, y: number): number;
+  }
+  const mySum: cal = (x, y) => {
+    return x + y;
+  };
+
+  //引數預設值
+  const buildName = (firstName: string, lastName: string = "cat"): string => {
+    return firstName + " " + lastName;
+  };
+
+  type calLength = (something: string | number) => number;
+  const getLength: calLength = (something) => {
+    if ((something as string).length) {
+      return (something as string).length;
+    } else {
+      return something.toString().length;
+    }
+  };
+  interface stringObject {
+    [propName: string]: string;
+  }
+  const allDept: stringObject[] = [
+    { name: "Steven", dept: "IT"},
+    { name: "John", dept: "Research" },
+    { name: "Mary", dept: "Sales" },
+  ];
+  type deptType = (arr: stringObject[]) => string;
+  const getDept: deptType = (arr) => {
+    let dept: string = "";
+    arr.forEach((item) => {
+      dept += "_" + item.dept;
+    });
+    return dept;
+  };
   return (
     <div>
       {Bot({ id: 1, name: "Steven", age: 18, job: "programer" })}
       <br />
       {sum(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)}
+      <br />
+      {getName("Steven")}
+      <br />
+      {mySum(1, 2)}
+      <br />
+      {getLength(1232)}
+      <br />
+      {getDept(allDept)}
     </div>
   );
 };
