9eccc7b9f59f60aadb2b820f3ace0ec9f09dfb99
steven31518 <107013204+steven31518@users.noreply.github.com>
2023-06-28 15:44:47 +0800

    Update Table.tsx

--- steven/src/Table.tsx
+++ steven/src/Table.tsx
@@ -10,15 +10,17 @@ const Table = ({ tableData }: tableDataProps) => {
       <thead></thead>
       <tbody>
         {tableData &&
-          tableData.map((item: any, index: number) => {
-            return (
-              <tr key={index}>
-                {Object.keys(item).map((key: string, index: number) => {
-                  return <td key={index}>{item[key]}</td>;
-                })}
-              </tr>
-            );
-          })}
+          tableData.map(
+            (item: { [propsName: string]: string }, index: number) => {
+              return (
+                <tr key={index}>
+                  {Object.keys(item).map((key: string, index: number) => {
+                    return <td key={index}>{item[key]}</td>;
+                  })}
+                </tr>
+              );
+            }
+          )}
       </tbody>
     </table>
   );
