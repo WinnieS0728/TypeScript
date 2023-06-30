620fc4846e6b84f829b8714097c311590b97e477
steven31518 <107013204+steven31518@users.noreply.github.com>
2023-06-30 11:49:53 +0800

    change files path

--- steven/src/App.tsx
+++ steven/src/App.tsx
@@ -1,12 +1,9 @@
 import React from "react";
-import Hello from "./Hello";
-import Practice from "./practice";
-import SalesVisit from "./SalesVisit";
+import { Hello, SalesVisit } from "./pages";
 
 function App() {
   return (
     <div className="App">
-      <Practice desc="React component with typescript">練習TS</Practice>
       <SalesVisit />
       <Hello />
     </div>
