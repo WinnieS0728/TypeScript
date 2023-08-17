bbd7bbba2f62c94dcbf1ff95eb73311998c753b7
steven31518 <107013204+steven31518@users.noreply.github.com>
2023-08-17 17:30:56 +0800

    mui/material 安裝 i18n 設定

--- winnies/src/i18n.ts
+++ winnies/src/i18n.ts
@@ -2,16 +2,15 @@ import i18n from "i18next";
 import { initReactI18next } from "react-i18next";
 import Backend from "i18next-http-backend";
 
-import common_en from "@locales/common/en.json"
-import customRatePage_en from "@locales/custom rate page/en.json"
-import settingPage_en from "@locales/setting page/en.json"
-import joyride_en from "@locales/joyride/en.json"
+import common_en from "@locales/common/en.json";
+import customRatePage_en from "@locales/custom rate page/en.json";
+import settingPage_en from "@locales/setting page/en.json";
+import joyride_en from "@locales/joyride/en.json";
 
-import common_tw from "@locales/common/zh.json"
-import customRatePage_tw from "@locales/custom rate page/zh.json"
-import settingPage_tw from "@locales/setting page/zh.json"
-import joyride_tw from "@locales/joyride/zh.json"
-
+import common_tw from "@locales/common/zh.json";
+import customRatePage_tw from "@locales/custom rate page/zh.json";
+import settingPage_tw from "@locales/setting page/zh.json";
+import joyride_tw from "@locales/joyride/zh.json";
 
 import LanguageDetector from "i18next-browser-languagedetector";
 
